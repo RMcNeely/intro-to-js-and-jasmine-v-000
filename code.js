2c05f742f28718ba02cfc6663d49216ae0ed59a0
Katrina McNeely <katrina@Katrinas-MacBook-Pro.local>
2016-05-29 18:12:24 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function favIceCream(icecream)  {
+  return `I love ${icecream}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(number)  {
+  return Math.floor(number)
+}
+
+function theTruth(truth) {
+  var x = truth
+  x = true
+  if (x) {
+    return x
+  }
+}
